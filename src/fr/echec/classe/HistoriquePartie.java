723e9719c80bb5564ce74c23e01e9e3d0d57f2b1
Francois Philippe <francoisphilippe67@gmail.com>
2022-07-29 12:17:32 +0200

    Modification de l'ecriture des coups

--- src/fr/echec/classe/HistoriquePartie.java
+++ src/fr/echec/classe/HistoriquePartie.java
@@ -1,5 +1,69 @@
 package fr.echec.classe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoriquePartie {
+	
+	/*	Liste de tous les coups
+	 *  Peut ajouter un coup
+	 *  Récupère le nom des joueurs
+	 *  Récupère l'id de la partie
+	 *  Indique le numero du coup
+	 */ 
+	
+	// Attributs
+	private int compteurCoup = 0;
+	
+	private List<String> listeCoups = new ArrayList<>();
+	
+	private String idJ1;
+	private String idJ2;
+	private String idPartie;
+	
+	// get-set methodes
+	
+	public int getCompteurCoup() {
+		return compteurCoup;
+	}
+	public void setCompteurCoup(int compteurCoup) {
+		this.compteurCoup = compteurCoup;
+	}
+	public List<String> getListeCoups() {
+		return listeCoups;
+	}
+	public void setListeCoups(List<String> listeCoups) {
+		this.listeCoups = listeCoups;
+	}
+	public String getIdJ1() {
+		return idJ1;
+	}
+	public void setIdJ1(String idJ1) {
+		this.idJ1 = idJ1;
+	}
+	public String getIdJ2() {
+		return idJ2;
+	}
+	public void setIdJ2(String idJ2) {
+		this.idJ2 = idJ2;
+	}
+	public String getIdPartie() {
+		return idPartie;
+	}
+	public void setIdPartie(String idPartie) {
+		this.idPartie = idPartie;
+	}
+	
+	// Methodes
+	
+	public void ajouterCoup(NotationCoup coup) {
+		String dernierCoup = coup.getCoupFormatStandard();
+		this.listeCoups.add(dernierCoup);
+	}
+	
+
+	
+	
+	
 
 }
--- src/fr/echec/classe/NotationCoup.java
+++ src/fr/echec/classe/NotationCoup.java
@@ -1,5 +1,98 @@
 package fr.echec.classe;
 
+import fr.echec.enumerateur.TypePiece;
+
 public class NotationCoup {
+	
+	/*	Récupère les coordonées de départ et d'arrivée de la pièce en format 0-63
+	 *  	NotationCoup coup1 = new NotationCoup(56,63)
+	 *  
+	 *  Conversion du format [0-63] en [a1-h8]
+	 *  	coup1.ecrireCoup();
+	 *  
+	 *  Dernier coup stocké dans coup1.coupFormatStandard
+	 *  	System.out.println(coup1.getCoupFormatStandard());
+	 */ 
+	
+	
+	// Attributs
+	private int coordDepart64;
+	private int coordArrivee64;
+	private String coordDepartStandard;
+	private String coordArriveeStandard;
+	private TypePiece typePiece;
+	private String coupFormatStandard;
+	
+	// constructeur
+	
+	public NotationCoup(int coordDepart64, int coordArrivee64) {
+		this.coordDepart64 = coordDepart64;
+		this.coordArrivee64 = coordArrivee64;
+	}
+	
+	// get-set methodes
+
+	public int getCoordDepart64() {return coordDepart64;}
+
+	public void setCoordDepart64(int coordDepart64) {this.coordDepart64 = coordDepart64;}
+
+	public int getCoordArrivee64() {return coordArrivee64;}
+
+	public void setCoordArrivee64(int coordArrivee64) {this.coordArrivee64 = coordArrivee64;}
+
+	public String getCoordDepartStandard() {return coordDepartStandard;}
+
+	public void setCoordDepartStandard(String coordDepartStandard) {this.coordDepartStandard = coordDepartStandard;}
+
+	public String getCoordArriveeStandard() {return coordArriveeStandard;}
+
+	public void setCoordArriveeStandard(String coordArriveeStandard) {this.coordArriveeStandard = coordArriveeStandard;}
+
+	public TypePiece getTypePiece() {
+		return typePiece;
+	}
+
+	public void setTypePiece(TypePiece typePiece) {
+		this.typePiece = typePiece;
+	}
+
+	public String getCoupFormatStandard() {
+		return coupFormatStandard;
+	}
+
+	public void setCoupFormatStandard(String coupFormatStandard) {
+		this.coupFormatStandard = coupFormatStandard;
+	}
+	
+	// Methodes
+	
+	
+	// TODO : obtenir le nom de la piece qui a bougé
+	public String nomPiece() {
+		return "";
+	}
+	
+	// Conversion du format 0-63 dans le format a1-h8
+	public String conversion(int coordonnee64) {
+		String coordonneeStandard = "";
+		
+		String[] listeLignes = new String[] {"a", "b", "c", "d", "e", "f", "g", "h"};
+		coordonneeStandard = listeLignes[coordonnee64 / 8] + (coordonnee64 % 8);
+		return coordonneeStandard;
+	}
+	
+	// Ecriture du dernier coup joué
+	public void ecrireCoup() {
+		// Récuperation des coordonnées standards
+		this.coordDepartStandard = this.conversion(coordDepart64);
+		this.coordArriveeStandard = this.conversion(coordArrivee64);
+		
+		// Ecriture du coup
+		this.coupFormatStandard = (this.nomPiece() +
+								   this.coordDepartStandard + "-" + 
+							       this.coordArriveeStandard);
+
+	}
+	
 
 }
