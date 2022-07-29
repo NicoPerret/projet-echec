72e3120c66ab1a7a19c44df0816a3dc8fc36954a
Fmmasson <francoismarie.masson@gmail.com>
2022-07-29 14:33:47 +0200

    ajout du constructeur Piece

--- src/fr/echec/classe/jeu/Piece.java
+++ src/fr/echec/classe/jeu/Piece.java
@@ -51,6 +51,14 @@ public class Piece {
 	public void setValeurMateriel(int valeurMateriel) {
 		this.valeurMateriel = valeurMateriel;
 	}
+	
+	// Constructeur pièce
+	
+	public Piece (TypePiece nom, boolean couleur, int valeurMateriel) {
+		
+	}
+	
+	
 
 // Méthodes
 
