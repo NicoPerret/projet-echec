099ca2e10102316307f27ade0d4139f1e9515988
Gortexxx <desmoulin.felix@gmail.com>
2022-07-29 12:03:22 +0200

    Chrono fonctionnel en milisecond

--- src/fr/echec/classe/jeu/Chrono.java
+++ src/fr/echec/classe/jeu/Chrono.java
@@ -1,45 +1,31 @@
 package fr.echec.classe.jeu;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Chrono {
 
+Timer chrono = new Timer();
+	
+public void decompte() {
 
-	private final long nanoSecondsPerSecond = 1000000000;
-	private final long nanoSecondsPerMinute = 60000000000L;
-
-
-	private long stopWatchStartTime = 0;
-	private long stopWatchStopTime = 0;
-	private boolean stopWatchRunning = false;
-
-	public void start() {
-		this.stopWatchStartTime = System.nanoTime();
-		this.stopWatchRunning = true;
-	}
-
-	public void stop() {
-		this.stopWatchStopTime = System.nanoTime();
-		this.stopWatchRunning = false;
-	}
-
-	public long getElapsedSeconds() {
-		long elapsedTime;
-
-		if (stopWatchRunning)
-			elapsedTime = (System.nanoTime() - stopWatchStartTime);
-		else
-			elapsedTime = (stopWatchStopTime - stopWatchStartTime);
-
-		return elapsedTime / nanoSecondsPerSecond;
-	}
-
-	public long getElapsedMinutes() {
-		long elapsedTime;
-		if (stopWatchRunning)
-			elapsedTime = (System.nanoTime() - stopWatchStartTime);
-		else
-			elapsedTime = (stopWatchStopTime - stopWatchStartTime);
-
-		return elapsedTime / nanoSecondsPerMinute;
-	}
-
+	chrono.schedule(new TimerTask() {
+		
+		int temps = 160;
+		@Override
+		public void run() {
+			 int tempsMinute = temps / 60; 
+			 int tempsSecond = temps % 60;
+			 
+			 System.out.println(tempsMinute + " : " + tempsSecond );
+			
+			if (temps == 0) {
+				System.out.println("Défaite au temps");
+				cancel();
+			}
+			temps--;
+			
+		}
+	}, 1000, 1000);
+}
 }
