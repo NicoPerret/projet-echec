297633afa568165f1d1a8c0545408585cb7b4755
Gortexxx <desmoulin.felix@gmail.com>
2022-07-29 17:24:25 +0200

    Correction du system Elo

--- src/fr/echec/classe/modeJeux/JcJ.java
+++ src/fr/echec/classe/modeJeux/JcJ.java
@@ -1,17 +1,21 @@
 package fr.echec.classe.modeJeux;
 
+import fr.echec.classe.joueur.Utilisateur;
+
 public class JcJ {
 	boolean couleurJoueur1;
 	boolean couleurJoueur2;
-	
-	
-	public void calculElo(int ra, int rb, double sa, double sb) {//mettre joueurs en parametre de la fct pour recup elo et le modifier
+
+	public void calculElo(Utilisateur joueur1, Utilisateur joueur2, double sa, double sb) {
+		// mettre joueurs en
+// parametre de la fct pour
+// recup elo et le modifier
 
 		// Real Rating Player A/B
 		// Elo des joueurs avant la partie
 		//
-		// float ra;
-		// float rb;
+		int ra = joueur1.getElo();
+		int rb = joueur2.getElo();
 
 		// Outcome of the game for Player A/B (1 for win / 0.5 for draw / 0 for loss)
 		// Résultat de la partie (1 pour victoire / 0.5 pour Nulle / 0 pour défaite)
@@ -40,28 +44,27 @@ public class JcJ {
 
 		ea = (float) (1 / (1 + (Math.pow(10, (rb - ra) / 400))));
 		eb = (float) (1 / (1 + (Math.pow(10, (ra - rb) / 400))));
-		
-		
+
 		// Calcul du nouveau Elo
-		int differentielA =   (int) Math.ceil(k * (sa - ea));
-		int differentielB = (int) Math.ceil(k* (sb - eb)) ;
-		
-		
+		int differentielA = (int) Math.ceil(k * (sa - ea));
+		int differentielB = (int) Math.ceil(k * (sb - eb));
+
 		ran = ra + differentielA;
 		rbn = rb + differentielB;
 
 		// Arrondie au superieur
 
-		int nouveauEloA =  (int) Math.ceil(ran);
-		int nouveauEloB = (int) Math.ceil(rbn);
+		joueur1.setElo((int) Math.ceil(ran)) ;
+		joueur2.setElo((int) Math.ceil(rbn));
+		
+		
 
-		System.out.println( " Ancien Elo Joueur A : " + ra );
-		System.out.println( " Ancien Elo Joueur B : " + rb );
+		System.out.println(" Ancien Elo Joueur A : " + ra);
+		System.out.println(" Ancien Elo Joueur B : " + rb);
 		System.out.println("Changement A : " + differentielA);
 		System.out.println("Changement B : " + differentielB);
-		System.out.println( " Nouveau Elo Joueur A : " + nouveauEloA );
-		System.out.println( " Nouveau Elo Joueur B : " + nouveauEloB );
-		
-		
+		System.out.println(" Nouveau Elo Joueur A : " + joueur1.getElo());
+		System.out.println(" Nouveau Elo Joueur B : " + joueur2.getElo());
+
 	}
 }
