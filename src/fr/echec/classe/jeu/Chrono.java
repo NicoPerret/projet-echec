c8922fb272bae61306c266ffb63b70142dafe674
Gortexxx <desmoulin.felix@gmail.com>
2022-08-03 17:28:39 +0200

    MAJ du chrono (oui encore)

--- src/fr/echec/classe/jeu/Chrono.java
+++ src/fr/echec/classe/jeu/Chrono.java
@@ -1,32 +1,102 @@
 package fr.echec.classe.jeu;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class Chrono {
 
-	Timer chrono = new Timer();
+class Chrono {
 
-	public void decompte() { //cancel n'arrete pas la fonction
+	private final long nanoSecondsPerSecond = 1000000000;
+	private final long nanoSecondsPerMinute = 60000000000L;
+	private long stopWatchStartTime = 0;
+	private long stopWatchStopTime = 0;
+	private boolean stopWatchRunning = false;
 
-		chrono.schedule(new TimerTask() {
 
-			int temps = 160;
+	public void start() {
+		this.stopWatchStartTime = System.nanoTime();
+		this.stopWatchRunning = true;
 
-			@Override
-			public void run() {
-				int tempsMinute = temps / 60;
-				int tempsSecond = temps % 60;
-
-				System.out.println(tempsMinute + " : " + tempsSecond);
-
-				if (temps == 0) {
-					System.out.println("Défaite au temps");
-					cancel();
-				}
-				temps--;
-
-			}
-		}, 1000, 1000);
 	}
+
+	public void stop() {
+		this.stopWatchStopTime = System.nanoTime();
+		this.stopWatchRunning = false;
+	}
+
+	public long getElapsedSeconds() {
+		long elapsedTime;
+
+		if (stopWatchRunning)
+			elapsedTime = (System.nanoTime() - stopWatchStartTime);
+		else
+			elapsedTime = (stopWatchStopTime - stopWatchStartTime);
+
+		return elapsedTime / nanoSecondsPerSecond;
+	}
+
+	public long getElapsedMinutes() {
+		long elapsedTime;
+		if (stopWatchRunning)
+			elapsedTime = (System.nanoTime() - stopWatchStartTime);
+		else
+			elapsedTime = (stopWatchStopTime - stopWatchStartTime);
+
+		return elapsedTime / nanoSecondsPerMinute;
+	}
+
+	public long getStopWatchStartTime() {
+		return this.stopWatchStartTime;
+	}
+
+	public void getTempsRestant(long tempsRestant) {
+		System.out.println("Temps restant " + tempsRestant / nanoSecondsPerMinute + " mintutes et "
+				+ (tempsRestant / nanoSecondsPerSecond) % 60 + " secondes");
+	}
+
+	
 }
+
+						// EXEMPLE D'UTILISATION 
+
+
+
+//public static void main(String[] args) {
+//
+//	Chrono chronoj1 = new Chrono();
+//	Chrono chronoj2 = new Chrono();
+//	long nanoSecondsPerSecond = 1000000000;
+//	long nanoSecondsPerMinute = 60000000000L;
+//	long tempsRestantJ1 = 600000000000L;
+//	long tempsRestantJ2 = 600000000000L;
+//	long temps = 0;
+//
+//	// Tour du J1
+//	chronoj1.start();
+//	for(int i = 0 ; i<2000000; i++) {
+//
+//		temps = System.nanoTime() - chronoj1.getStopWatchStartTime();
+//
+//		if (temps > tempsRestantJ1) {
+//			System.out.println("Perdu au temps");
+//			break;
+//		}
+//	}
+//	chronoj1.stop();
+//	tempsRestantJ1 -= temps;
+//	chronoj1.getTempsRestant(tempsRestantJ1);
+//
+//	// Tour du J2
+//
+//	chronoj2.start();
+//	while (true) {
+//		
+//		temps = System.nanoTime() - chronoj2.getStopWatchStartTime();
+//		
+//		if (temps > tempsRestantJ2) {
+//			System.out.println("Perdu au temps");
+//			break;
+//		}
+//	}
+//	chronoj2.stop();
+//	tempsRestantJ2 -= temps;
+//	chronoj1.getTempsRestant(tempsRestantJ2);
+//}
