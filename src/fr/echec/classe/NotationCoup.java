67014180f2f4d674bbf2a28f0b1a19146a6751ff
Gortexxx <desmoulin.felix@gmail.com>
2022-08-22 09:36:05 +0200

    Correction de NotationCoup

--- src/fr/echec/classe/NotationCoup.java
+++ src/fr/echec/classe/NotationCoup.java
@@ -3,18 +3,17 @@ package fr.echec.classe;
 import fr.echec.enumerateur.TypePiece;
 
 public class NotationCoup {
-	
-	/*	Récupère les coordonnées de départ et d'arrivée de la pièce en format 0-63
-	 *  	NotationCoup coup1 = new NotationCoup(56,63);
-	 *  
-	 *  Conversion du format [0-63] en [a1-h8]
-	 *  	coup1.ecrireCoup();
-	 *  
-	 *  Dernier coup stocké dans coup1.coupFormatStandard
-	 *  	System.out.println(coup1.getCoupFormatStandard());
-	 */ 
-	
-	
+
+	/*
+	 * Récupère les coordonnées de départ et d'arrivée de la pièce en format 0-63
+	 * NotationCoup coup1 = new NotationCoup(56,63);
+	 * 
+	 * Conversion du format [0-63] en [a1-h8] coup1.ecrireCoup();
+	 * 
+	 * Dernier coup stocké dans coup1.coupFormatStandard
+	 * System.out.println(coup1.getCoupFormatStandard());
+	 */
+
 	// Attributs
 	private int coordDepart64;
 	private int coordArrivee64;
@@ -22,31 +21,47 @@ public class NotationCoup {
 	private String coordArriveeStandard;
 	private TypePiece typePiece;
 	private String coupFormatStandard;
-	
+
 	// constructeur
-	
+
 	public NotationCoup(int coordDepart64, int coordArrivee64) {
 		this.coordDepart64 = coordDepart64;
 		this.coordArrivee64 = coordArrivee64;
 	}
-	
+
 	// get-set methodes
 
-	public int getCoordDepart64() {return coordDepart64;}
+	public int getCoordDepart64() {
+		return coordDepart64;
+	}
 
-	public void setCoordDepart64(int coordDepart64) {this.coordDepart64 = coordDepart64;}
+	public void setCoordDepart64(int coordDepart64) {
+		this.coordDepart64 = coordDepart64;
+	}
 
-	public int getCoordArrivee64() {return coordArrivee64;}
+	public int getCoordArrivee64() {
+		return coordArrivee64;
+	}
 
-	public void setCoordArrivee64(int coordArrivee64) {this.coordArrivee64 = coordArrivee64;}
+	public void setCoordArrivee64(int coordArrivee64) {
+		this.coordArrivee64 = coordArrivee64;
+	}
 
-	public String getCoordDepartStandard() {return coordDepartStandard;}
+	public String getCoordDepartStandard() {
+		return coordDepartStandard;
+	}
 
-	public void setCoordDepartStandard(String coordDepartStandard) {this.coordDepartStandard = coordDepartStandard;}
+	public void setCoordDepartStandard(String coordDepartStandard) {
+		this.coordDepartStandard = coordDepartStandard;
+	}
 
-	public String getCoordArriveeStandard() {return coordArriveeStandard;}
+	public String getCoordArriveeStandard() {
+		return coordArriveeStandard;
+	}
 
-	public void setCoordArriveeStandard(String coordArriveeStandard) {this.coordArriveeStandard = coordArriveeStandard;}
+	public void setCoordArriveeStandard(String coordArriveeStandard) {
+		this.coordArriveeStandard = coordArriveeStandard;
+	}
 
 	public TypePiece getTypePiece() {
 		return typePiece;
@@ -63,28 +78,26 @@ public class NotationCoup {
 	public void setCoupFormatStandard(String coupFormatStandard) {
 		this.coupFormatStandard = coupFormatStandard;
 	}
-	
+
 	// Methodes
 
 	// Conversion du format 0-63 dans le format A1-H8
-	// attention les cases ne sont pas encore déterminées
+	
 	public String conversion(int coordonnee64) {
 		String coordonneeStandard = "";
-		
-		String[] listeLignes = new String[] {"A", "B", "C", "D", "E", "F", "G", "H"};
-		coordonneeStandard = listeLignes[coordonnee64 % 8] + (coordonnee64 / 8);
+
+		String[] listeLignes = new String[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+		coordonneeStandard = listeLignes[coordonnee64 % 8] + (coordonnee64 / 8 + 1);
 		return coordonneeStandard;
 	}
-	
+
 	// Ecriture du dernier coup joué
 	public void ecrireCoup() { // A COMPLETER
 		// Récuperation des coordonnées standards
 		this.coordDepartStandard = this.conversion(coordDepart64);
 		this.coordArriveeStandard = this.conversion(coordArrivee64);
-		
+
 		// Ecriture du coup
-		this.coupFormatStandard = (this.typePiece +
-								   this.coordDepartStandard + "-" + 
-							       this.coordArriveeStandard);
+		this.coupFormatStandard = (this.typePiece + this.coordDepartStandard + "-" + this.coordArriveeStandard);
 	}
 }
