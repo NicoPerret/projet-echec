32506760874da3de5267ed1964349eb15b9b22fc
Fmmasson <francoismarie.masson@gmail.com>
2022-09-02 15:23:49 +0200

    fonction roque + save

--- src/main/java/fr/echec/application/Application.java
+++ src/main/java/fr/echec/application/Application.java
@@ -2,6 +2,7 @@ package fr.echec.application;
 
 import fr.echec.classe.Partie;
 import fr.echec.classe.modeJeux.ParametresPartie;
+import fr.echec.service.HistoriquePartieService;
 
 public class Application {
 
@@ -10,7 +11,7 @@ public class Application {
 		// SETUP RUDIMENTAIRE MAIS FONCTIONNEL
 		ParametresPartie param = new ParametresPartie();
 		Partie p = new Partie(param);
-		
+		HistoriquePartieService srvHistPartie = new HistoriquePartieService();
 		// UNE PARTIE
 		while (true) {
 
---
+++ src/main/java/fr/echec/repository/jpa/AbstractRepository.java
@@ -0,0 +1,110 @@
+package fr.echec.repository.jpa;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class AbstractRepository<T> {
+	protected static EntityManagerFactory emf = null;
+	
+private Class<T> clz;
+	
+	public AbstractRepository(Class<T> clz) {
+		if (emf == null) { // Singleton (garantir qu'on aura qu'une seule instance dans toute l'application)
+			emf = Persistence.createEntityManagerFactory("EshopUnit");
+		}
+		
+		this.clz = clz;
+	}
+	public List<T> findAll() {
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			return em
+					.createQuery("select e from " + clz.getSimpleName() + " e", clz)
+					.getResultList();
+		}
+		
+		catch (Exception e) {
+			return null;
+		}
+		
+		finally {
+			em.close();
+		}
+	}
+	public T findById(Integer id) {
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			return em.find(clz, id);
+		}
+		
+		catch (Exception e) {
+			return null;
+		}
+		
+		finally {
+			em.close();
+		}
+	}
+	
+	public void save(T entity, boolean insert) {
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			em.getTransaction().begin();
+			
+			if (insert) {
+				em.persist(entity);
+			}
+			
+			else {
+				em.merge(entity);
+			}
+			
+			em.getTransaction().commit();
+		}
+		
+		catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		finally {
+			em.close();
+		}
+	}
+
+	public void deleteById(Integer id) {
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			em.getTransaction().begin();
+			
+			em	.createQuery("delete from " + clz.getSimpleName() + " e where e.id = ?1")
+				.setParameter(1, id)
+				.executeUpdate();
+			
+			em.getTransaction().commit();
+		}
+		
+		catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		finally {
+			em.close();
+		}
+	}
+	
+	// Permet de fermer l'EMF depuis l'application, à la fin de l'application par exemple
+	public static void close() {
+		if (emf != null) {
+			emf.close();
+		}
+	}
+}
+
