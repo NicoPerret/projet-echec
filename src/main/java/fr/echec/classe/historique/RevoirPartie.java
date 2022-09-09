2bcc80b6401d76d282442f07a8a9447fb4cdfdc2
Francois Philippe <francoisphilippe67@gmail.com>
2022-09-09 15:29:45 +0200

    .

--- src/main/java/fr/echec/classe/historique/RevoirPartie.java
+++ src/main/java/fr/echec/classe/historique/RevoirPartie.java
@@ -3,21 +3,34 @@ package fr.echec.classe.historique;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.echec.classe.jeu.Fen;
+import fr.echec.classe.jeu.Plateau;
+import fr.echec.classe.parametres.ParametresPartie;
 import fr.echec.service.HistoriquePartieService;
 
 @Service
 public class RevoirPartie {
+	
 	@Autowired
 	private HistoriquePartieService srvHistPartie;
+	
+	@Autowired
+	protected Fen fen;
+	@Autowired
+	protected Plateau plateau;
+	
+	ParametresPartie param = new ParametresPartie();
 	
 	private HistoriquePartie histPartie;
 	
 	
 	public void trouveHistoriquePartie(int id) {
 		try {
+			System.out.println(id);
 			this.histPartie = srvHistPartie.findById(id);
 		}
 		catch (Exception e) {
+			System.out.println(e);
 			e.printStackTrace();
 		}
 	}
@@ -29,19 +42,25 @@ public class RevoirPartie {
 		System.out.println("------------ joueurs -------------");
 		System.out.println("Joueur 1 : " + this.histPartie.getJ1().getPseudo()
 							+ " (elo : " + this.histPartie.getJ1().getElo() + ")"
-							+ " joue les " + this.histPartie.getJ1().getCouleur().toString());
+							+ " joue les " + this.histPartie.getJ1().getCouleur());
 		System.out.println("Joueur 2 : " + this.histPartie.getJ2().getPseudo()
 							+ " (elo : " + this.histPartie.getJ1().getElo() + ")"
-							+ " joue les " + this.histPartie.getJ1().getCouleur().toString());
+							+ " joue les " + this.histPartie.getJ1().getCouleur());
 		System.out.println("------------- date ---------------");
 		System.out.println(this.histPartie.getDate());
 		System.out.println("-------- Paramètres partie -------");
-		System.out.println(this.histPartie.getParam().toString()); // Faut voir quoi mettre...
+		System.out.println(this.histPartie.getParam()); // Faut voir quoi mettre...
 		System.out.println();
 		
 	}
 	
+	
 	public void generationPartie() {
+		
+		// Récupérer le fen de départ dans param
+		String fen_depart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+		this.plateau = fen.creationPlateau(fen_depart);
+		// System.out.println(plateau);
 		// Generer le plateau, joueurs, ...
 	}
 	
