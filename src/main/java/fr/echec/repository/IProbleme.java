ff4f1eba0d3e40a98d50206b0c714a55fe834c58
Fmmasson <francoismarie.masson@gmail.com>
2022-09-09 17:13:02 +0200

    ajout difficulte dans probleme

--- src/main/java/fr/echec/repository/IProbleme.java
+++ src/main/java/fr/echec/repository/IProbleme.java
@@ -1,9 +1,15 @@
 package fr.echec.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.echec.classe.probleme.Probleme;
 
 public interface IProbleme extends JpaRepository<Probleme, Integer> {
 	public Probleme findById(int id);
+	
+	@Query(value ="select p from Probleme p where p.difficulte between ?1 and ?2")
+	public List<Probleme> FindByDifficultyBetween (float a, float b);
 }
