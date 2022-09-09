74fdc8d9d598849359c8046b2e4f18287c8b5d30
NicoPerret <nico.p18500@gmail.com>
2022-09-09 12:27:25 +0200

    pb

--- src/main/java/fr/echec/repository/IProbleme.java
+++ src/main/java/fr/echec/repository/IProbleme.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.echec.classe.probleme.Probleme;
 
 public interface IProbleme extends JpaRepository<Probleme, Integer> {
-
+	public Probleme findById(int id);
 }
