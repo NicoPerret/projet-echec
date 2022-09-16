407f6bbcf1dccee112fd3dc1d18cc25fbeb42b1a
NicoPerret <nico.p18500@gmail.com>
2022-09-16 16:55:45 +0200

    review a 4 html

--- HTML/assets/js/jeu.js
+++ HTML/assets/js/jeu.js
@@ -2,8 +2,7 @@ import { timerJ1, stopJ1, stopJ2, secJ1,secJ2 } from "./chronos.js";
 
 const couleurJoueur = "blanc"
 const audio = new Audio('assets/372.mp3');
-
-const typePartie = "1v1";
+const typePartie = "1v2";
 
 for (let i = 0; i < 8; i++) {
     const c = document.createElement('div');
