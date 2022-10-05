e542afe277a91a6adfaf524d06a45842e32fdeff
Gortexxx <desmoulin.felix@gmail.com>
2022-10-05 12:28:48 +0200

    tests divers

--- AngularProjetEchec/src/app/projet/component/jeu/jeu.component.ts
+++ AngularProjetEchec/src/app/projet/component/jeu/jeu.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jeu',
   templateUrl: './jeu.component.html',
-  styleUrls: ['./jeu.component.css']
+  styleUrls: ['./jeu.component.css'],
 })
 export class JeuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
