322abd2fd8ab636c18de6a64f9b0dafa30032e9e
Gortexxx <desmoulin.felix@gmail.com>
2022-10-11 17:27:56 +0200

    Rame

--- AngularProjetEchec/src/assets/scriptDragDrop.js
+++ AngularProjetEchec/src/assets/scriptDragDrop.js
@@ -1,87 +1,86 @@
+let possibilites = [];
+let coordArrivee;
 function dragDrop(coordonnee, plateau) {
-  let possibilites = [];
-  let cpt = 1;
-  let bool = true;
-  let audio = new Audio('assets/372.mp3');
-  console.log(document.querySelectorAll('img'));
-  for (let i of document.querySelectorAll('img')) {
-    i.addEventListener('dragstart', (e) => {
-      e.dataTransfer.setData('piece-id', e.target.id);
+  (async () => {
+    let cpt = 1;
+    let bool = true;
+    let audio = new Audio("assets/372.mp3");
 
-      // possibilites = [];
-      // possiblites = trouverCoupsPossibles(coordonnee);
+    for (let i of document.querySelectorAll("img")) {
+      i.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("piece-id", e.target.id);
 
-      // possibilites.push('C3', 'C2', 'F1', 'A5');
+        const options = {
+          method: "POST",
+          body: JSON.stringify(plateau),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
 
-      // ${plateau}&
-console.log(plateau);
-      const options = {
-        method: 'POST',
-        body: plateau,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      (async () => {
-        possibilites = await fetch(
-          `http://localhost:8080/projet-echecs/api/coup-possible/${coordonnee}`,
-          options
-        ).then((resp) => resp.json());
-      })();
+        (async () => {
+          possibilites = await fetch(
+            `http://localhost:8080/projet-echecs/api/coup-possible/${coordonnee}`,
+            options
+          ).then((resp) => resp.json());
+        })();
 
-      if (bool) {
-        bool = false;
-        // timerJ1();
-      }
-      for (let pos of possibilites) {
-        document.querySelector(`#${pos}`).style.background = 'green';
-      }
-    });
-  }
+        for (let pos of possibilites) {
+          document.querySelector(`#${pos}`).style.background = "green";
+        }
+        if (bool) {
+          bool = false;
+          // timerJ1();
+        }
+      });
+    }
 
-  for (let div of document.querySelectorAll('.grillePlateauPrincipal div')) {
-    div.addEventListener('dragenter', (e) => {
-      e.preventDefault();
+    for (let div of document.querySelectorAll(".grillePlateauPrincipal div")) {
+      div.addEventListener("dragenter", (e) => {
+        e.preventDefault();
 
-      if (possibilites.find((pos) => pos === e.target.getAttribute('id'))) {
-        e.target.style.background = 'red';
-      }
-    });
+        if (possibilites.find((pos) => pos === e.target.getAttribute("id"))) {
+          e.target.style.background = "red";
+        }
+      });
 
-    div.addEventListener('dragleave', (e) => {
-      e.preventDefault();
-      if (possibilites.find((pos) => pos === e.target.getAttribute('id'))) {
-        e.target.style.background = 'green';
-      }
-    });
+      div.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        if (possibilites.find((pos) => pos === e.target.getAttribute("id"))) {
+          e.target.style.background = "green";
+        }
+      });
 
-    div.addEventListener('dragover', (e) => {
-      e.preventDefault();
-    });
+      div.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
 
-    div.addEventListener('drop', (e) => {
-      const target = e.target.closest('div');
-      console.log(target);
+      div.addEventListener("drop", (e) => {
+        const target = e.target.closest("div");
+        console.log(target);
 
-      if (possibilites.find((pos) => pos === target.getAttribute('id'))) {
-        const pieceId = e.dataTransfer.getData('piece-id');
-        const piece = document.querySelector(`#${pieceId}`);
+        if (possibilites.find((pos) => pos === target.getAttribute("id"))) {
+          const pieceId = e.dataTransfer.getData("piece-id");
+          const piece = document.querySelector(`#${pieceId}`);
+          coordArrivee = pieceId;
+          target.innerHTML = "";
+          target.append(piece);
+          //exécution bruit POC
+          audio.play();
 
-        target.innerHTML = '';
-        target.append(piece);
-        //exécution bruit POC
-        audio.play();
-
-        /* if (cpt % 2 == 0) {
+          /* if (cpt % 2 == 0) {
           stopJ2();
         } else {
           stopJ1();
         }
         cpt++;*/
-      }
-      for (let pos of possibilites) {
-        document.querySelector(`#${pos}`).style.background = '';
-      }
-    });
-  }
+        }
+        for (let pos of possibilites) {
+          document.querySelector(`#${pos}`).style.background = "";
+        }
+        possibilites = [];
+      });
+    }
+  })();
+  return [coordonnee, coordArrivee];
 }
---
+++ src/main/java/fr/echec/demowebsocket/Coords.java
@@ -0,0 +1,16 @@
+package fr.echec.demowebsocket;
+
+import java.util.List;
+
+public class Coords {
+	private List<String> coup;
+
+	public List<String> getCoup() {
+		return coup;
+	}
+
+	public void setCoup(List<String> coup) {
+		this.coup = coup;
+	}
+
+}
